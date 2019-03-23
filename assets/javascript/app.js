079457d13d15b666e84ff0b15399434df561d0bb
Robert Butler <robwbutler@gmail.com>
2019-03-22 22:07:37 -0400

    added real questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,102 +11,102 @@ var incorrect = "";
 
 var list = {
     0: {
-        question: "Who's buried in Grant's tomb",
-        choice1: "A) Tomby McTombface",
-        choice2: "B) Lee",
-        choice3: "C) Grant",
-        choice4: "D) It actually symbolizes all Grants",
-        correct: "C) Grant",
+        question: "Which of these is the closest living relative to humans?",
+        choice1: "A) Pigs",
+        choice2: "B) Gorillas",
+        choice3: "C) Chimpanzees",
+        choice4: "D) Orangutans",
+        correct: "C) Chimpanzees",
     
     },
     1: {
 
-        question: "Who's buried in Lee's tomb",
-        choice1: "A) Bomby McBombface",
-        choice2: "B) Grant",
-        choice3: "C) Lee",
-        choice4: "D) It actually symbolizes all Lees",
-        correct: "C) Lee",
+        question: "Humans belong to what genus?",
+        choice1: "A) Humanum",
+        choice2: "B) Hominidae",
+        choice3: "C) Homo",
+        choice4: "D) Manus",
+        correct: "C) Homo",
     
     },
     2: {
 
-        question: "Who's buried in Bomby's tomb",
-        choice1: "A) Bomby McBombface",
-        choice2: "B) Grant",
-        choice3: "C) Lee",
-        choice4: "D) It actually symbolizes all Lees",
-        correct: "A) Bomby McBombface",
+        question: "This ancient hominin was first discovered with a specimen known as 'Taung Child'.",
+        choice1: "A) Australopithecus africanus",
+        choice2: "B) Homo neanderthalis",
+        choice3: "C) Cro Magnon",
+        choice4: "D) Homo habilis",
+        correct: "A) Australopithecus africanus",
     
     },
     3: {
 
-        question: "Who's buried in Bee's tomb",
-        choice1: "A) Gomby McLombface",
-        choice2: "B) Brant",
-        choice3: "C) Gee",
-        choice4: "D) It actually symbolizes all Bees",
-        correct: "D) It actually symbolizes all Bees",
+        question: "This Australopithicus specimen, discovered in the 1970s, was named after a contemporary song.",
+        choice1: "A) Roxannne",
+        choice2: "B) Jolene",
+        choice3: "C) Sharona",
+        choice4: "D) Lucy",
+        correct: "D) Lucy",
     
     },
     4: {
 
-        question: "Who's buried in Zee's tomb",
-        choice1: "A) Gomby McGombface",
-        choice2: "B) Zrant",
-        choice3: "C) Zee",
-        choice4: "D) It actually symbolizes all Zees",
-        correct: "C) Zee",
+        question: "This 'English ape' was regarded as legitimate for years by British paleontologists before it was exposed as a hoax.",
+        choice1: "A) Cheddar Man",
+        choice2: "B) Ice Man",
+        choice3: "C) Piltdown Man",
+        choice4: "D) Mega Man",
+        correct: "C) Piltdown Man",
     
     },
     5: {
 
-        question: "Who's buried in Frampt's tomb",
-        choice1: "A) Fromby McFrombface",
-        choice2: "B) Frampt",
-        choice3: "C) Free",
-        choice4: "D) It actually symbolizes all Frampts",
-        correct: "B) Frampt",
+        question: "This early human species is named for its upright posture.",
+        choice1: "A) Homo verticalus",
+        choice2: "B) Homo erectus",
+        choice3: "C) Homo bipedalis",
+        choice4: "D) Homo sapiens",
+        correct: "B) Homo erectus",
     
     },
     6: {
 
-        question: "Who's buried in Pant's tomb",
-        choice1: "A) Pomby McPombface",
-        choice2: "B) Pant",
-        choice3: "C) Pee",
-        choice4: "D) It actually symbolizes all Pants",
-        correct: "B) Pant",
+        question: "Which of these is a benefit of bipedalism?",
+        choice1: "A) Conserving energy while walking",
+        choice2: "B) Freeing of hands for carrying objects",
+        choice3: "C) Able to stand taller and see farther",
+        choice4: "D) All of the above",
+        correct: "D) All of the above",
     
     },
     7: {
 
-        question: "Who's buried in Jee's tomb",
-        choice1: "A) Jomby McJombface",
-        choice2: "B) Jrant",
-        choice3: "C) Jee",
-        choice4: "D) It actually symbolizes all Jees",
-        correct: "A) Jomby McJombface",
+        question: "Homo floresiensis, a debated ancient species of human from Indonesia, was given the following nickname for its short stature:",
+        choice1: "A) Hobbit",
+        choice2: "B) Gnome",
+        choice3: "C) Gremlin",
+        choice4: "D) Lilliputian",
+        correct: "A) Hobbit",
     
     },
     8: {
 
-        question: "Who's buried in Yee's tomb",
-        choice1: "A) Yomby McYombface",
-        choice2: "B) Yrant",
-        choice3: "C) Yee",
-        choice4: "D) It actually symbolizes all Yees",
-        correct: "C) Yee",
+        question: "These ancient humans coexisted alongside our ancestors, and were named for the region in France they were found.",
+        choice1: "A) Cro Magnons",
+        choice2: "B) Parisians",
+        choice3: "C) Neanderthals",
+        choice4: "D) Morlocks",
+        correct: "C) Neanderthals",
     
     },
     9: {
 
-        question: "Who's buried in Vrant's tomb",
-        choice1: "A) Vomby McVombface",
-        choice2: "B) Vrant",
-        choice3: "C) Vee",
-        choice4: "D) It actually symbolizes all Vrants",
-        correct: "B) Vrant",
+        question: "Humans considered virtually identical to our modern selves and not ancient ancestors are generally referred to as:",
+        choice1: "A) Post-Stone Age humans",
+        choice2: "B) Anatomically modern humans",
+        choice3: "C) Real people",
+        choice4: "D) Post-diluvians",
+        correct: "B) Anatomically modern humans",
     
     },
 }
